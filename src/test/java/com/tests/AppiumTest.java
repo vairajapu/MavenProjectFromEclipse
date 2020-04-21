f6d5524de2e8d5d5bdb9704d7d51c4d6e7c9965f
Vaishali <vaishalibakale@gmail.com>
2020-04-21 17:07:37 +0530

    Initial commit

---
+++ src/test/java/com/tests/AppiumTest.java
@@ -0,0 +1,12 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void openBrowser()
+	{
+		System.out.println("Opening Browser");
+	}
+
+}
---
+++ src/test/java/com/tests/SeleniumTest.java
@@ -0,0 +1,19 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void doLogin()
+	{
+		System.out.println("Executing Login test");
+	}
+	
+	@Test
+	public void userRegistration()
+	{
+		System.out.println("Registering User");
+	}
+	
+}
